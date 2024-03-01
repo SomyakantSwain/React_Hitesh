b5fc0883fcd7f730dfbca94fa9b239b44e45968a
SomyakantSwain <somyakanta9@gmail.com>
2024-03-01 07:00:01 +0530

    loader added

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/GIthub/Github.jsx'
+import Github, { githubInfoLoader } from './components/GIthub/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -38,7 +38,7 @@ const router = createBrowserRouter(
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
       <Route path='github'
-        l
+        loader={githubInfoLoader}
 
         element={<Github />} />
     </Route>
@@ -47,6 +47,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} /> // it needs a prop
+    <RouterProvider router={router} /> // router provider needs a it needs a prop
   </React.StrictMode>,
 )
